135abced80a96acbe9eabd81f2b337617f742088
mlisi1 <m.lisi1@phd.unipi.it>
2026-04-01 14:52:32 +0200

    aliases are now in utilities.sh

---
+++ docker/utilities.sh
@@ -0,0 +1,4 @@
+source /opt/ros/jazzy/setup.bash
+source /home/ros/install/setup.bash
+source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+alias no_gpu='__NV_PRIME_RENDER_OFFLOAD=0 __GLX_VENDOR_LIBRARY_NAME='
